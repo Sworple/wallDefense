a52750135699c3d5305645439b19abdaede59b90
sworple <145728862+Sworple@users.noreply.github.com>
2024-08-30 13:49:38 -0800

    aaaaAAAAAAAAAAAAAAAAAAAAAAA

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
-let projX = 0;
-let projSpeed = 7;
-let projActive = false;
-let time = 0;
+let projSpeed = 5;
 let enemySpeed = 2;
+let projectile, badGuy;
+let time = 0;
+let score = 0;
 
 function  preload(){
 
@@ -14,18 +14,21 @@ function setup() {
   turret = new Sprite();
   turret.fill = 'orange';
   turret.diameter = 90;
+  enemySpawn();
 }
 function draw() {
 	clear();
 	background(0,0,75);
-  //the turret itself
-
   turret.x = 0;
   turret.y = mouseY;
+  fill('orange')
 	stroke('orange')
 	rect(0, mouseY-15, 90, 30)
 	stroke('black');
     fill('black');
+    textSize(10);
+    textFont('Comic Sans MS')
+    text(score, 50, mouseY+5)
     circle(90, mouseY, 30)
 
     if(frameCount === 60){
@@ -36,26 +39,30 @@ function draw() {
     time = 0;
     enemySpawn();
     }
-    if(mouseIsPressed === true){
+    if(mouse.presses()){
       projectile = new Sprite();
       projectile.color = 'grey';
       projectile.stroke = 'black';
       projectile.diameter = 30;
-      projX = 85;
-    }
-    if(projX = 85){
+      projectile.speed = projSpeed;
       projectile.y = mouseY;
-    }
-    if(projX >= canvas.w+40){
-      projectile = remove();
+      projectile.x = 90;
+      projectile.mass = 5000;
+      projectile.collided(badGuy, scoreUp)
     }
 }
-
 function enemySpawn(){
   for(let i = 1; i < random(1,3); i++){
     badGuy = new Sprite();
     badGuy.diameter = 30;
     badGuy.x = canvas.w + (random(50, 100))
     badGuy.y = random(50, canvas.h-50)
+    badGuy.direction = 180;
+    badGuy.speed = enemySpeed;
   }
+}
+function scoreUp(){
+  projectile.remove;
+  badGuy.remove;
+  score++;
 }
