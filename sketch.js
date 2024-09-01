d425a50267a8875086b9fcad9e28914fa2400374
Sworple <smuggreyboi@hotmail.com>
2024-08-31 21:58:24 -0800

    clear up index.html and 'maybe fixed' the actual code

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 let projSpeed = 5;
 let enemySpeed = 2;
-let projectile, badGuy;
+let projectile, badGuy, proj, enemy;
 let time = 0;
 let score = 0;
 
@@ -11,10 +11,29 @@ function setup() {
 	Canvas('16:9');
 	noCursor();
 	frameRate(60);
+  //turret
   turret = new Sprite();
   turret.fill = 'orange';
   turret.diameter = 90;
+
+  //projectile setup
+  proj = new Group();
+  proj.color = 'grey';
+  proj.stroke = 'black';
+  proj.diameter = 30;
+  proj.speed = projSpeed;
+  proj.mass = 500;
+
+
+  enemy = new Group();
+  enemy.diameter = 30;
+  enemy.direction = 180;
+  enemy.speed = enemySpeed;
+
+  //not calling enemySpawn in the setup breaks the game.
+  //don't ask me how, i don't know either.
   enemySpawn();
+  proj.collided(enemy, scoreUp);
 }
 function draw() {
 	clear();
@@ -40,29 +59,24 @@ function draw() {
     enemySpawn();
     }
     if(mouse.presses()){
-      projectile = new Sprite();
-      projectile.color = 'grey';
-      projectile.stroke = 'black';
-      projectile.diameter = 30;
-      projectile.speed = projSpeed;
-      projectile.y = mouseY;
-      projectile.x = 90;
-      projectile.mass = 5000;
-      projectile.collided(badGuy, scoreUp)
+      projectileSpawn();
     }
+}
+function projectileSpawn(){
+  projectile = new proj.Sprite();
+  projectile.y = mouseY;
+  projectile.x = 90;
+
 }
 function enemySpawn(){
   for(let i = 1; i < random(1,3); i++){
-    badGuy = new Sprite();
-    badGuy.diameter = 30;
-    badGuy.x = canvas.w + (random(50, 100))
-    badGuy.y = random(50, canvas.h-50)
-    badGuy.direction = 180;
-    badGuy.speed = enemySpeed;
+    badGuy = new enemy.Sprite();
+    badGuy.x = canvas.w + (random(50, 100));
+    badGuy.y = random(50, canvas.h-50);
   }
 }
 function scoreUp(){
-  projectile.remove;
-  badGuy.remove;
+  projectile.remove();
+  badGuy.remove();
   score++;
 }
